708203d3eb641689f9fc2068326e0d442e51bbc0
PeterQuadrini <quadrinipietro@gmail.com>
2023-11-17 17:23:04 +0100

    links

--- static/app/app.js
+++ static/app/app.js
@@ -5,12 +5,41 @@ function chiedi() {
     let loader = document.getElementById("loader")
     let index = Math.floor(Math.random() * IT.length);
     let phrase = IT[index]
-    cmp.innerHTML = phrase
+    //cmp.innerHTML = phrase
+    cmp.innerHTML = 'Rimuovi le tue incertezze'
     loader.style.display = 'inline-block'
     cmp.style.display = 'none'
     setTimeout(() => {  
         cmp.style.display = 'inline-block'
         loader.style.display = 'none'
+        
+    }, 3000);
+}
+
+function chiedi2() {
+    let cmp = document.getElementById("scritta")
+    let cmp2 = document.getElementById("scritta2")
+    let cmp3 = document.getElementById("scritta3")
+    let loader = document.getElementById("loader")
+    let loader2 = document.getElementById("loader2")
+    let loader3 = document.getElementById("loader3")
+    cmp.innerHTML = 'Continua sulla strada che hai scelto'
+    cmp2.innerHTML = 'La vita ti sta preparando qualcosa di grande'
+    cmp3.innerHTML = 'Lavora su te stesso'
+    //cmp.innerHTML = "Qualsiasi cosa tu faccia durera' a lungo"
+    loader.style.display = 'inline-block'
+    loader2.style.display = 'inline-block'
+    loader3.style.display = 'inline-block'
+    cmp.style.display = 'none'
+    cmp2.style.display = 'none'
+    cmp3.style.display = 'none'
+    setTimeout(() => {  
+        cmp.style.display = 'inline-block'
+        cmp2.style.display = 'inline-block'
+        cmp3.style.display = 'inline-block'
+        loader.style.display = 'none'
+        loader2.style.display = 'none'
+        loader3.style.display = 'none'
         
     }, 3000);
 }
